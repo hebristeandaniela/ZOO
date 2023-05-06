dda5c09703dda35d028dd244d0343ad85da105c2
Hebristean Daniela <daniela.hebristean22@gmail.com>
2023-05-06 18:51:45 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+            <div class="footer-area">
+                <p>Zoo Management System @ 2022. All right reserved</p>
+            </div>
+        </footer>
---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    
+    
+<!--header-->
+            <div class="header">
+                <div class="container">
+                    <div class="header-top">
+                        <nav class="navbar navbar-default">
+                            <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+                                <div class="navbar-header">
+                                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                      </button>
+                                    <div class="navbar-brand">
+                                        <h1><a href="index.php">New Delhi Zoological Park</a></h1>
+                                    </div>
+                                </div>
+                    
+                        
+    <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav">
+                                    <li><a href="index.php"><i class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a></li>
+                                    <li><a href="about.php"><i class="far fa-address-card"></i>About</a></li>
+                                    <li><a href="contact.php"><i class="fa fa-cog fa-spin"></i>Contact</a></li>
+                                    <li><a href="animals.php"><i class="fas fa-paw"></i>Animals</a></li>
+                                    <li><a href="admin/index.php"> <i class="fa fa-user"></i>Admin</a></li>
+                                </ul>
+                              
+                            </div> <!--/.navbar-collapse --> 
+                           
+                
+                         </div><!-- /.container-fluid -->
+                        </nav>
+
+                    </div>
+                </div>
+            </div>
